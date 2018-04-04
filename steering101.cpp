a5869ff5cd75db0606dddd8842c20480eaa48ed1
Jens Bouman <jens@retailunity.com>
2018-04-04 09:14:29 +0200

    Restructure steering101

--- steering101.cpp
+++ steering101.cpp
@@ -10,7 +10,6 @@ void exit_signal_handler(int signo);
 
 BrickPi3 BP;
 
-
 void calibrate(vector<sensor_color_t> & colors, vector<sensor_light_t> & lights){
 	sensor_color_t Color1;
 	sensor_light_t Light3;
@@ -33,6 +32,112 @@ void calibrate(vector<sensor_color_t> & colors, vector<sensor_light_t> & lights)
 	}
 }
 
+void printCalibration(const vector<sensor_color_t> & v){
+	cout << "Color info: " << endl;
+	int lowestRed = -1;
+	int highestRed = -1;
+	for(unsigned int i = 0; i < v.size(); i++){
+		if(lowestRed == -1 || v[i].reflected_red < lowestRed){
+			lowestRed = v[i].reflected_red;
+		}
+
+		if(highestRed == -1 || v[i].reflected_red > highestRed){
+			highestRed = v[i].reflected_red;
+		}
+	}
+	cout << "    Min color: " << lowestRed << endl;
+	cout << "    Max color: " << highestRed << endl;
+	cout << "    highest difference: " << highestRed - lowestRed << endl;
+}
+
+void printCalibration(const vector<sensor_light_t> & v){
+	cout << "Light info: " << endl;
+	int lowestAmbientLight = -1;
+	int highestAmbientLight = -1;
+	int lowestReflected = -1;
+	int highestReflected = -1;
+	for(unsigned int i = 0; i < v.size(); i++){
+		if(lowestAmbientLight == -1 || v[i].ambient < lowestAmbientLight){
+			lowestAmbientLight = v[i].ambient;
+		}
+		if(highestAmbientLight == -1 || v[i].ambient > highestAmbientLight){
+			highestAmbientLight = v[i].ambient;
+		}
+
+		if(lowestReflected == -1 || v[i].reflected < lowestReflected){
+			lowestReflected = v[i].reflected;
+		}
+		if(highestReflected == -1 || v[i].reflected > highestReflected){
+			highestReflected = v[i].reflected;
+		}
+	}
+	cout << "    Min ambient: "  << lowestAmbient << endl;
+	cout << "    Max ambient: "  << highestAmbient << endl;
+	cout << "    Ambient difference: "  << highestAmbient - lowestAmbient << endl;
+	cout << "    Min reflect: "  << lowestReflected << endl;
+	cout << "    Max reflect: "  << highestReflected << endl;
+	cout << "    Reflect difference: "  << highestReflected - lowestReflected << endl;
+	cout << endl;
+}
+
+void startSteering(BrickPi3 controller){
+	sensor_light_t Light3;
+	sensor_color_t Color1;
+   // current date/time based on current system
+	time_t now = time(0);
+
+	int counter = 0;
+	int power = -20;
+	while(true){
+
+		// left sensor right motor
+		controller.get_sensor(PORT_3, Light3);
+		uint16_t val = Light3.reflected;
+		if(val < lowestReflected) val = lowestReflected;
+		if(val > highestReflected) val = highestReflected;
+		int16_t rightmotorpower = (100*(val - lowestReflected))/(highestReflected - lowestReflected);
+
+		controller.get_sensor(PORT_4, Color1);
+		val = Color1.reflected_red;
+		if(val < lowestAmbient) val = lowestAmbient;
+		if(val > highestReflected) val = highestAmbient;
+		int16_t leftmotorpower = (100*(val - highestAmbient))/(lowestAmbient- highestAmbient);
+
+		// right sensor left motor
+//		if(leftmotorpower < 10 && rightmotorpower < 10){
+//			controller.set_motor_power(PORT_B,power);
+//			controller.set_motor_power(PORT_C,power);
+//		}else{
+			controller.set_motor_power(PORT_B, power + (rightmotorpower*-1));
+			controller.set_motor_power(PORT_C, power + (leftmotorpower*-1));
+//		}
+		cout << "motors: " << leftmotorpower << " - " << rightmotorpower << endl;
+		cout << endl;
+		counter++;
+		if(counter == 100){
+			break;
+		}
+	}
+}
+
+void startSteering2(BrickPi3 controller){
+	sensor_color_t steerColor;
+	sensor_light_t steerLight;
+
+	float tollerance = 1.07; // in percentage
+	int moveRightMotor = 50;
+	int moveLeftMotor = 50;
+	
+	while(true){
+		if(controller.get_sensor(PORT_3, steerColor) == 0){
+			
+		}
+		if(controller.get_sensor(PORT_4, steerLight) == 0){
+		
+		}
+	}
+}
+
 int main(){
 	signal(SIGINT, exit_signal_handler);
 
@@ -48,103 +153,13 @@ int main(){
 	vector<sensor_light_t> lights = {};
 
 	calibrate(colors, lights);
-
-	cout << "Color info: " << endl;
-	int lowestAmbient = -1;
-	int highestAmbient = -1;
-	for(unsigned int i = 0; i < colors.size(); i++){
-		if(lowestAmbient == -1 || colors[i].reflected_red < lowestAmbient){
-			lowestAmbient = colors[i].reflected_red;
-		}
-		if(highestAmbient == -1 || colors[i].reflected_red > highestAmbient){
-			highestAmbient = colors[i].reflected_red;
-		}
-	}
-	cout << "    Min color: " << lowestAmbient << endl;
-	cout << "    Max color: " << highestAmbient << endl;
-	cout << "    highest difference: " << highestAmbient - lowestAmbient << endl;
-
-	cout << "Light info: " << endl;
-	int lowestAmbientLight = -1;
-	int highestAmbientLight = -1;
-	int lowestReflected = -1;
-	int highestReflected = -1;
-	for(unsigned int i = 0; i < lights.size(); i++){
-		if(lowestAmbientLight == -1 || lights[i].ambient < lowestAmbientLight){
-			lowestAmbientLight = lights[i].ambient;
-		}
-		if(highestAmbientLight == -1 || lights[i].ambient > highestAmbientLight){
-			highestAmbientLight = lights[i].ambient;
-		}
-
-		if(lowestReflected == -1 || lights[i].reflected < lowestReflected){
-			lowestReflected = lights[i].reflected;
-		}
-		if(highestReflected == -1 || lights[i].reflected > highestReflected){
-			highestReflected = lights[i].reflected;
-		}
-	}
-	cout << "    Min ambient: "  << lowestAmbient << endl;
-	cout << "    Max ambient: "  << highestAmbient << endl;
-	cout << "    Ambient difference: "  << highestAmbient - lowestAmbient << endl;
-	cout << "    Min reflect: "  << lowestReflected << endl;
-	cout << "    Max reflect: "  << highestReflected << endl;
-	cout << "    Reflect difference: "  << highestReflected - lowestReflected << endl;
-
-	cout << endl;
+	displayCalibration(colors);
+	displayCalibration(lights);
+		
 	sleep(5);
 
-// Deciding to steer
-/* 
-	sensor_color_t steerColor;
-	sensor_light_t steerLight;
-
-	float tollerance = 1.07; // in percentage
-	while(true){
-		int moveRightMotor = -50;
-		int moveLeftMotor = -50;
-		if(BP.get_sensor(PORT_3, steerColor) == 0){
-			if(steerColor.reflected_red > (lowestAmbient * tollerance) ){
-				moveLeftmotor -= 10;
-			}
-		}
-		if(BP.get_sensor(PORT_4, steerLight) == 0){
-			if(steerLight.reflected){
-			
-			}
-		}
-	}
-*/
-	sensor_light_t Light3;
-	sensor_color_t Color1;
-
-	int power = -20;
-	while(true){
-
-		// left sensor right motor
-		BP.get_sensor(PORT_3, Light3);
-		uint16_t val = Light3.reflected;
-		if(val < lowestReflected) val = lowestReflected;
-		if(val > highestReflected) val = highestReflected;
-		int16_t rightmotorpower = (100*(val - lowestReflected))/(highestReflected - lowestReflected);
-
-		BP.get_sensor(PORT_4, Color1);
-		val = Color1.reflected_red;
-		if(val < lowestAmbient) val = lowestAmbient;
-		if(val > highestReflected) val = highestAmbient;
-		int16_t leftmotorpower = (100*(val - highestAmbient))/(lowestAmbient- highestAmbient);
-
-		// right sensor left motor
-//		if(leftmotorpower < 10 && rightmotorpower < 10){
-//			BP.set_motor_power(PORT_B,power);
-//			BP.set_motor_power(PORT_C,power);
-//		}else{
-			BP.set_motor_power(PORT_B, power + (rightmotorpower*-1));
-			BP.set_motor_power(PORT_C, power + (leftmotorpower*-1));
-//		}
-		cout << "motors: " << leftmotorpower << " - " << rightmotorpower << endl;
-		cout << endl;
-	}
+	startSteering(BP); // v1
+	//startSteering2(BP); //v2
 }
 
 void exit_signal_handler(int signo){
