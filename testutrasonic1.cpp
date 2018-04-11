d19ad24a80c6fe8a30fa8674e6e215586f5b43cb
UEVarol <36413624+UEVarol@users.noreply.github.com>
2018-04-11 13:54:15 +0200

    object detection and turning
    In this code, we worked on the left and right turn of the robot. This code is clear in contrast with the first source code. We made a function for the left and right turn, so it will be more simple to understand the code.

--- testutrasonic1.cpp
+++ testutrasonic1.cpp
@@ -18,43 +18,83 @@ int16_t ultrasoon_detectie() {
 	return val;
 }
 
+
+void steering(string direction){
+	if(direction == "left"){
+		BP.set_motor_position_relative(PORT_B, -650);
+		BP.set_motor_position_relative(PORT_C, 650);
+	}else if(direction == "right"){
+		BP.set_motor_position_relative(PORT_B, 650);
+		BP.set_motor_position_relative(PORT_C, -650);
+	}
+}
+
+int test= BP.reset_motor_encoder(PORT_D);
+
 int main(){
  
     signal(SIGINT, exit_signal_handler);
-// een limiet 
-	BP.set_motor_limits(PORT_B, 60, 0);
-	BP.set_motor_limits(PORT_D, 60, 0);
+// een limietinstellen wanneer de  motors een bocht gaat maken gebruikt die normaal de volle kracht 
+	//BP.set_motor_limits(PORT_B, 30, 0);
+	//BP.set_motor_limits(PORT_C, 30, 0);
+	//BP.set_motor_limits(PORT_D, 60, 0);
 	BP.detect(); 
 
   BP.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_ULTRASONIC);
   BP.get_sensor(PORT_2, afstand);
 
 int16_t detectie;
+bool is_turret_rotated;
 while(true){
     detectie = ultrasoon_detectie();
-    if(detectie >= 30){
-//draai de onderkant van de tank terug als die niet meer het object ziet. kanon blijft nu gericht op de zijkant van het object
-BP.set_motor_position_relative(PORT_B,  90);
-BP.set_motor_position_relative(PORT_C, -90);
-       BP.set_motor_power(PORT_B, -20);
-       BP.set_motor_power(PORT_C, -20);
+   int32_t turret_positie = BP.get_motor_encoder(PORT_D);
+   cout << "test " << turret_positie << endl;
+	if(turret_positie > -225 && turret_positie < -215){
+		is_turret_rotated = true;
+	}else{
+		is_turret_rotated = false;
+	}
 
+	if(detectie >= 30){
+	cout << "detectie = " << detectie << endl;
+//draai de onderkant van de tank terug als die niet meer het object ziet. kanon blijft nu gericht op de zijkant van het object
+	if(is_turret_rotated){
+		cout << "turret rotated & detectie >= 30" << endl;
+		steering("right");
+	BP.set_motor_position_relative(PORT_B, 4*-360);
+	BP.set_motor_position_relative(PORT_C, 4*-360);
+	sleep(5);
+	}else{
+
+	BP.set_motor_position_relative(PORT_B, -720);
+	BP.set_motor_position_relative(PORT_C, -720);
+	//steering("right");
+	//BP.set_motor_position_relative(PORT_B, -1080);
+	//BP.set_motor_position_relative(PORT_C, -1080);
+	}
     }
     else if( detectie >=25 && detectie <30 ){
+	cout << " detectie = " << detectie << endl;
+	if(is_turret_rotated){
+		cout << "gogogogo"<< endl;
+	}else{
+	steering("left");
+	//BP.set_motor_position_relative(PORT_B, -360);
 
-BP.set_motor_position(PORT_B,-90);
-BP.set_motor_position(PORT_C, 90);
-// turret
-BP.set_motor_position(PORT_D,-120);
-// wacht tot onderkant en turret is gedraait
-sleep(1);
-// rijden waneer die 25 cm afstand heeft met de konon gericht op het opject
-BP.set_motor_power(PORT_B, -20);
-BP.set_motor_power(PORT_C, -20);
-	}
-    else if(detectie <20){
+	// turret
+	BP.set_motor_position(PORT_D,-220);
+	// wacht tot onderkant en turret is gedraait
+	sleep(3);
+	// rijden waneer die 25 cm afstand heeft met de konon gericht op het opject
 	BP.set_motor_power(PORT_B, -20);
-	BP.set_motor_power(PORT_C, -25);
+	BP.set_motor_power(PORT_C, -20);
+	}
+	}
+
+    else if(detectie <25){
+	cout << "help" << endl;
+ 	BP.set_motor_power(PORT_B, -20);
+	BP.set_motor_power(PORT_C, -20);
 	}
 
  usleep(100000);//sleep 100 ms  
