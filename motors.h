458501848d56efb2213fd3e4e7f1d2d3c044070b
Jens Bouman <10463991+JensBouman@users.noreply.github.com>
2018-04-13 12:20:34 +0200

    Version 1.1 gridsteering to target
    This version can follow to the given target.

--- motors.h
+++ motors.h
@@ -23,14 +23,35 @@ void startSteering(BrickPi3 BP, const sensorData & sensorReads, bool obstacles);
 /**
  * @param BP The BrickPi3 controller
  * @param direction The direction the robot has to turn to
- *
+ * @param steps The ammount of steps the function needs to make
+ * @param sleepCount The ammount the robot needs to sleep for in seconds, default value of 1 second
+ * 
  * This function wil turn the robot on it's axis.
  */
 void steering(BrickPi3 BP, const string & direction, const int & steps, const float & sleepCount = 1.0);
 
+/**
+ * @param BP The BrickPi3 controller
+ * @param sensorReads The struct of the calibration data
+ * 
+ * This function will detect objects infront of the robot
+ */
 void detectObject(BrickPi3 BP, const sensorData & sensorReads);
 
+/** 
+ * @param BP The BrickPi3 controller
+ * @param sensorReads The struct of the calibration data
+ * 
+ * Swerve is the function that will dodge any objects the robots sees in the detectObject function.
+ */
 void swerve(BrickPi3 BP, const sensorData & sensorReads);
 
+/**
+ * @param BP The BrickPi3 controller
+ * @param turnDirection The direction the robot wil turn to
+ * @param sensorReads The struct of the calibration data
+ * 
+ * This function will try to find a line using previously acquired data in the direction given.
+ */
 void findLine(BrickPi3 BP, string turnDirection, const sensorData & sensorReads);
 #endif
